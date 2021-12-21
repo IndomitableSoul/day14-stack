cfe2b10070cf27f0f3a856fcd8af50c31323708f
nikita <amarnikita09@gmail.com>
2021-12-21 14:02:18 +0530

    Refactored MyStack.java

--- src/com/stack/MyStack.java
+++ src/com/stack/MyStack.java
@@ -8,12 +8,20 @@ public class MyStack {
 		/* UC1: Ability to create a Stack of 56->30->70 */
 		Stack<Integer> stack = new Stack<>();
 		
-		/*
+		/* UC:1
 		 * 70 will be added first then 30 and then 56 to make 56 on top of the Stack
 		 */
 		stack.push(70);
 		stack.push(30);
 		stack.push(56);
+		
+		/* UC:2
+		 * Ability to peak and pop from the Stack till it is empty
+		 */
+		System.out.println(stack.peek());
+		while(!stack.isEmpty()) {
+			System.out.println(stack.pop());
+		}
 		
 	}
 }
