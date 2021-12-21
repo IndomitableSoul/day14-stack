bd4c23e0ddcea8e99a191502cfb61c54073d6dcf
nikita <amarnikita09@gmail.com>
2021-12-21 13:48:06 +0530

    Added MyStack.java

---
+++ src/com/stack/MyStack.java
@@ -0,0 +1,14 @@
+package com.stack;
+
+import java.util.Stack;
+
+public class MyStack {
+    public static void main(String[] args) {
+    	//UC1: Ability to create a Stack of 56->30->70
+		Stack<Integer> stack = new Stack<>();
+		stack.push(70);
+		stack.push(30);
+		stack.push(56);
+		
+	}
+}
