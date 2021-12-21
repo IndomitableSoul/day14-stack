2b239e074a9ff2bb4caf412f6f4c9b4fe6898604
nikita <amarnikita09@gmail.com>
2021-12-21 13:51:07 +0530

    ceated branch UC1

--- src/com/stack/MyStack.java
+++ src/com/stack/MyStack.java
@@ -4,8 +4,13 @@ import java.util.Stack;
 
 public class MyStack {
     public static void main(String[] args) {
-    	//UC1: Ability to create a Stack of 56->30->70
+    	
+		/* UC1: Ability to create a Stack of 56->30->70 */
 		Stack<Integer> stack = new Stack<>();
+		
+		/*
+		 * 70 will be added first then 30 and then 56 to make 56 on top of the Stack
+		 */
 		stack.push(70);
 		stack.push(30);
 		stack.push(56);
