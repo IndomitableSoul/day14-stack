e0e5591499b553096f159ffa1da8791a8e79d8c9
IndomitableSoul <53757659+IndomitableSoul@users.noreply.github.com>
2021-12-22 12:01:53 +0530

    created new repo for linkedlist problem statement

--- src/com/linkedList/MyLinkedList.java
+++
@@ -1,9 +0,0 @@
-package com.linkedList;
-
-import java.util.LinkedList;
-
-public class MyLinkedList {
-		public static void main(String[] args) {
-			LinkedList<Integer> list = new LinkedList<Integer>();
-		}
-}
